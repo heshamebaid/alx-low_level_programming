5b5f166789166e2d10fa292b9a55494fad22261b
Hesham <hesham.ebaid@ejust.edu.eg>
2023-08-15 19:44:24 -0700

    ffff

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
 	int i, j;
@@ -21,9 +25,3 @@ void jack_bauer(void)
 		i++;
 	}
 }
-
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- */
-
